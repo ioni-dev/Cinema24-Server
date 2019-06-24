0299248b73a0819e16d2a6778ef5beb98bcf33d4
ioni <ioni_dev@protonmail.com>
2019-06-24 19:06:46 -0300

    new code for db and routes, new user model and cod

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const app = express();
 // connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
+
+// Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 const PORT = process.env.PORT || 5000;
 
